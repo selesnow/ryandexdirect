9e41e27bf06ba83e9f4fc4cf357a277253f2700b
Alexey Seleznev <selesnow@mail.ru>
2018-02-12 12:41:40 +0200

    Add files via upload

---
+++ utf8/R/yadirStartKeyWords.R
@@ -0,0 +1,77 @@
+yadirStartKeyWords <-  function(Login = NULL, 
+                                Ids   = NULL,
+                                Token = NULL){
+  
+  #Проверка заполнения токена
+  if(is.null(Token)) {
+    stop("Enter your API token!")
+  }
+  
+  if(length(Ids) > 10000){
+    stop(paste0("В параметр Ids переданы номера ",length(Ids), " ключевых слов, максимально допустимое количество ключевых слов в одном запросе 10000."))
+  }
+  
+  if(is.null(Ids)){
+    stop("В аргумент Ids необходимо передать вектор содержаший Id ключевых слов по которым необходимо возобновить показ. Вы не передали ниодного Id.")
+  }
+  
+  #Счётчик ошибок
+  CounErr <- 0
+  
+  #Error vector
+  errors_id <-  vector()
+  
+  #Фиксируем время начала работы
+  start_time  <- Sys.time()
+  
+  #Задаём начальный offset
+  lim <- 0
+  
+  #Сообщение о начале обработки данных
+  packageStartupMessage("Processing", appendLF = T)
+  
+  IdsPast <- paste0(Ids, collapse = ",")
+  #Формируем тело POST запроса
+  queryBody <- paste0("{
+                      \"method\": \"resume\",
+                      \"params\": { 
+                      \"SelectionCriteria\": {
+                      \"Ids\": [",IdsPast,"]}
+}
+}")
+  
+  #Отправка запроса
+  answer <- POST("https://api.direct.yandex.com/json/v5/keywords", body = queryBody, add_headers(Authorization = paste0("Bearer ",Token), 'Accept-Language' = "ru","Client-Login" = Login))
+  #Парсим ответ
+  ans_pars <- content(answer)
+  #Проверка ответа на наличие ошибки
+  if(!is.null(ans_pars$error)){
+    stop(paste0("Ошибка: ", ans_pars$error$error_string,". Сообщение: ",ans_pars$error$error_detail, ". ID Запроса: ",ans_pars$error$request_id))
+  }
+  
+  #Проверка необработанных кампаний
+  for(error_search in 1:length(ans_pars$result$ResumeResults)){
+    if(!is.null(ans_pars$result$ResumeResults[[error_search]]$Errors)){
+      CounErr <- CounErr + 1
+      errors_id <- c(errors_id, Ids[error_search])
+      packageStartupMessage(paste0("    KeyWordId: ",Ids[error_search]," - ", ans_pars$result$ResumeResults[[error_search]]$Errors[[1]]$Details))
+    }
+  }
+  
+  #Подготовка сообщения про количество остановленных кампаний
+  out_message <- ""
+  
+  TotalCampStoped <- length(Ids) - CounErr
+  
+  if(TotalCampStoped %in% c(2,3,4) & !(TotalCampStoped %% 100 %in% c(12,13,14))){
+    out_message <- "ключевых слова запущено"
+  } else if(TotalCampStoped %% 10 == 1 & TotalCampStoped %% 100 != 11){
+    out_message <- "ключевое слово запущено"
+  } else {
+    out_message <- "ключевых слов запущено"
+  }
+  
+  #Выводим информацию
+  packageStartupMessage(paste0(TotalCampStoped, " ", out_message))
+  packageStartupMessage(paste0("Общее время работы функции: ", as.integer(round(difftime(Sys.time(), start_time , units ="secs"),0)), " сек."))
+  return(errors_id)}
